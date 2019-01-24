463192720ea586e83f9dc99747e31906a0acff7c
DrQuack <47011925+DrQuack@users.noreply.github.com>
2019-01-24 14:16:17 -0700

    Create ProjectMain.cpp

---
+++ ProjectMain.cpp
@@ -0,0 +1,13 @@
+/*
+* Main .cpp file for the final project
+* Authors: David Bonney [Everyone else add names here]
+* Date: 1/24/2019
+* Version Number: 1.0 (Please update this when you make edits/updates)
+* 
+*/
+
+int main ()
+{
+//TODO Add stuff per requirement
+return 1; //1 indicates proper exit
+}
