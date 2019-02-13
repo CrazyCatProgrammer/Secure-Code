4929ff69b1aeae09f357184a097ace31d893a6d6
DrQuack <47011925+DrQuack@users.noreply.github.com>
2019-02-13 14:48:26 -0700

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -3,38 +3,32 @@
  *
  *  Created on: Feb 3, 2019
  *      Author: David Bonney
- *		Edited by: Robyn Collins
  */
 
 #ifndef CUSTOMER_H_
 #define CUSTOMER_H_
 
-#include <iostream>
 #include<string>
+#include<iostream>
 
 class Customer {
 public:
 	//Constructors and Destructors
-
-
-	Customer(std::string name = "", double price = 0); //make sure the 0 sentinel value is in constructor logic
+	Customer(std::string name = "", float price = 0); //make sure the 0 sentinel value is in constructor logic
 	//No destructor needed as no heap memory used
   //Member Functions
 		//getters & setters
-
 	std::string getName() { return name; }
 	double getPrice() { return price; }
-//	std::string getName() { return name; }
-//	double getPrice() { return price; }
 	void setName(std::string input);
-	void setPrice(int input);
+	void setPrice(float input);
 	//prints
 	void printName();
 	void printPrice();
 	void printAll();
 
 private:
-	double price;
+	float price;
 	std::string name;
 };
 
