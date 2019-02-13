d8ec28e8e21b2b86e3c114a75584ef13d7f3bb1e
DrQuack <47011925+DrQuack@users.noreply.github.com>
2019-02-13 14:48:51 -0700

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,6 +1,6 @@
 #include "Customer.h"
 
-Customer::Customer(std::string name, double price)
+Customer::Customer(std::string name, float price)
 {
 	setName(name);
 	setPrice(price);
@@ -11,19 +11,19 @@ void Customer::setName(std::string input)
 	name = input;
 }
 
-void Customer::setPrice(int input)
+void Customer::setPrice(float input)
 {
 	price = input;
 }
 
 void Customer::printName()
 {
-	std::cout << "Name: " << name;
+	std::cout << "Name: " << name << '\n';
 }
 
 void Customer::printPrice()
 {
-	std::cout << "Price: " << price;
+	std::cout << "Price: " << price << '\n';
 }
 
 void Customer::printAll()
