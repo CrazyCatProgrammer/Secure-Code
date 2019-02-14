0b254f1b60e66fa27f3edda2d99350b84c08a21f
Roxa <meowcollins@gmail.com>
2019-02-14 15:38:45 -0700

    Fixed Erros in Program
    Fixed menu infinite error loop. Name and price are only taking respective 10 and 6 chars entered. print also works.

--- ProjectMain.cpp
+++ ProjectMain.cpp
@@ -1,39 +1,10 @@
-/* Assignment #1 (pre-test) code
-* Started 1/31/2019
-*
-* Team members: David Bonney,
-*
-* Directions:
-*
-* Develop the design and code for each of the following programs
-*
-*   Program #1
-*     Write a program that generates a sequence of twenty random values between 0 and 99 in an array. Print the sequence, sort the array, and print the sorted sequence.
-*
-*   Program #2
-*     A supermarket wants to reward its best customer of each day, showing the customer’s name on a screen in the supermarket.
-*     For that purpose, the customers’ purchase amount is stored in an array and the customer’s name is stored
-*     in a corresponding array.
-*     Write a program that prompts the cashier to enter all prices and names, then adds them to two arrays,
-*     calls the function that you implemented, and displays the result. Use a price of 0 as a sentinel.
-*
-* Each program should include COMMENTS (internal), and a structured (functions).
-* Each function should indicate who completed the design and code, or sections of the code.
-*
-* Modify each of the programs to include an example  the following:
-*   Mitigation strategies for strings and string handling
-*   String handling functions
-*   Dynamic Allocation functions
-*   Runtime Protection Strategies
-*   Stack Canaries
-*   Null Pointers
-*   Allocation Functions
-*
-* Version number 1.00
-*
-* Program 1: Carson, Bonnie
-* Program 2: Robyn, David, Paul
-*/
+/*
+ * Project.cpp
+ *
+ *  Created on:
+ *      Author: Paul Grubb
+ *		Edited by: Robyn Collins & David Bonney
+ */
 
 #define fstackprotectorall //stack canary turned on for all functions. A bit overkill, but works.
 #pragma warning(disable : 4996)
@@ -62,42 +33,34 @@ void ShowData(CustomerRewards &accounts) //reworked by David
 	cout << "\nYour best Customer is:\n";
 	accounts.printBest();
 }
-/*
-void AskforData(CustomerRewards accounts)
-{
-	int stop;
-	cout << "\nplease enter customer data\n"
-	<< "please enter customer data below\n\n";
-	cout << "enter amount\n";
-	while (scanf("%f", &den) == 0 || den == 0)
-	while (cin >> &accounts.CustomerAmountArray[accounts.NumberOfCustomers] == 0 || den == 0) {}
-	cin >> accounts.CustomerAmountArray[accounts.NumberOfCustomers];
-	cout << "enter name\n";
-	cin >> accounts.CustomerNameArray[accounts.NumberOfCustomers];
-	cout << accounts.CustomerNameArray[accounts.NumberOfCustomers] << " and " << accounts.CustomerAmountArray[accounts.NumberOfCustomers] << "\n";
-	accounts.NumberOfCustomers++;
-}*/
+
 
 void AskForData(CustomerRewards &accounts) {
 	Customer tempCust;
-	char holder [10];
+	char holder[10]; // for name
+	char holy[6]; //for price
 	float price = 0.0;
 	string name = "";
-	cin.ignore(); //it skips first getline, so ignore used
+	//cin.clear();
 	cout << "Please enter data for a customer:\n";
 	cout << "Name:\n";
-	cin.getline(holder, 10); //cuts off 10 characters for name. 10 should be plenty
+	fgets(holder, 10, stdin); //Tip from paul, fgets is WAY easier than .get
+
+	//cin.get(holder, 10); //cuts off 10 characters for name. 10 should be plenty
+	//cin.ignore(1000, '\n'); //it skips first getline, so ignore used
 	name = holder;
 	tempCust.setName(name);
 	while (price <= 0.0) //uses 0.0 sentinel value, as well as default of strtof to prevent non-inputs
 	{
 		cout << "Please enter price:\n";
-		cin.getline(holder, 6); //6 characters should be enough for a float
-		price = strtof(holder, NULL); //will store 0.0 if invalid conversion
-	}						
+		cin.clear();
+		cin.get(holy, 6); //6 characters should be enough for a float
+		cin.ignore(1000, '\n');
+		price = strtof(holy, NULL); //will store 0.0 if invalid conversion
+	}
 	tempCust.setPrice(price);
 	accounts.add(tempCust);
-	
+
 }
 
 int main() {
@@ -107,7 +70,9 @@ int main() {
 	int errorCount = 0;
 	while (errorCount < 3) {
 		cout << "Hello this app is our costumer database, to enter a new customer press 1.\nTo see a list of costumers press 2\nPress 0 to exit.\n";
-		cin >> input;
+		cin.get(input);
+		cin.ignore(1000, '\n');
+		cin.clear();
 		if (input == '1')
 		{
 			AskForData(accounts);
@@ -125,10 +90,7 @@ int main() {
 			errorCount++;
 			cout << "There was an error with that input, please try again.\n";
 		}
-		
+
 	}
 	return -1; //-1 indicated bad exit
 }
-
-//runtime protection strategies. https://stackoverflow.com/questions/8015355/are-c-strings-and-streams-buffer-overflow-safe
-// http://www.informit.com/articles/article.aspx?p=2036582&seqNum=6
