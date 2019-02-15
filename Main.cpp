d52126f8908004b6a38b60318023d8bb531fa2ff
Roxa <meowcollins@gmail.com>
2019-02-14 22:31:39 -0700

    Random Commit
    Github was being grumpy so I'm sorta commiting this?

--- Main.cpp
+++ Main.cpp
@@ -1,89 +1,267 @@
+//============================================================================
 
-#include "stdafx.h"
+//
+
+// Name        : Pretest_Question1.cpp
+
+// Author      : Bonnie Rogers, Carson Sharpless
+
+// Version     : 1.0.0
+
+// Date        : February 7th, 2019
+
+// Description : generates a sequence of twenty random values between 0 and 99 in an array.
+
+//				 Print the sequence, sort the array, and print the sorted sequence.
+
+// Functions   : zzzz
+
+//
+
+//============================================================================
+
+
+
+
+
+#define fstackprotectorall //stack canary 
+
 #include <iostream>
+
 #include <string>
+
+#include <algorithm>  	// for sort function
+
+#include <stdlib.h>	// rand function
 
 using namespace std;
 
-class NameAmount
+
+
+
+
+/////////////////////////////////////////// Function Call ///////////////////////////////////////////
+
+
+
+int generate(int s[20], const int l);
+
+void print(int s[20], const int l);
+
+int sort(int s[20], const int l);
+
+
+
+//create and destroy
+
+
+
+int createArray(const int l);
+
+int destroyArray(int in, const int l);
+
+
+
+
+
+/////////////////////////////////////////// Main ///////////////////////////////////////////
+
+
+
+int main()
+
 {
-public:
-	string CustomerNameArray[10];
-	int CustomerAmountArray[10];
-	int NumberOfCustomers = 0;
 
-};
+    //setting constant size for array
 
-void AskforData(NameAmount);
-void showData(NameAmount);
-void FlushInputBuffer(NameAmount);
-void OverWriteData(NameAmount);
+    const int l = 20;
 
-void OverWriteData(NameAmount accounts)
-{
-	for (int i = 0; i < 10; i++)
-	{
-		accounts.CustomerAmountArray[i] = 0;
-		//cout << "\noverright number " << i;
-	}
+    int s[l];
+
+    
+
+    //static function pointer for print
+
+    static void(*funcPrint)(int s[20], const int l);
+
+    funcPrint = &print;
+
+    
+
+    //creating array
+
+    s[l] = generate(s, l);
+
+    //print original array unsorted
+
+    print(s, l);
+
+    //sort array
+
+    sort(s, l);
+
+    //print array sorted
+
+    print(s, l); 
+
+    //set array to null
+
+    destroyArray(s[l], l);
+
+    
+
+    //ending program
+
+    char end = NULL;	//character to end program
+
+    cout << "End Program. Press any key to continue... ";
+
+    cin >> end;
+
+    
+
+    return 0;
+
+
+
 }
 
-void FlushInputBuffer(NameAmount)
+
+
+/////////////////////////////////////////// Functions ///////////////////////////////////////////
+
+
+
+//create array and calls generate function
+
+// l is length of array to create
+
+
+
+int createArray(const int l)
+
 {
-	int c;
-	while ((c = getchar()) != '\n' && c != EOF);  // remove bad characters from the input buffer
+
+    cout << "Creating Array of " << l << " length...\n";
+
+    int s[20];
+
+    
+
+    cout << "Generating numbers and putting into array...\n";
+
+    s[l] = generate(s, l);
+
+
+
+    return s[l];
+
 }
-void showData(NameAmount accounts)
+
+
+
+
+
+// sets array to null
+
+// in is array to set null
+
+
+
+int destroyArray(int in, int l)
+
 {
-	cout << "\n\n showing Custumer Data\n\n";
-	for (int i = 0; i < accounts.NumberOfCustomers; i++)
-	{
-		cout << "Name:			Amount:\n\n";
-		cout << accounts.CustomerNameArray[i].data() << "			";
-		cout << accounts.CustomerAmountArray[i] << "\n";
-		//cout << "\n\n" << accounts.NumberOfCustomers << "\n\n";
 
-	}
+    cout << "Setting array to null...\n";
+
+    in = NULL;
+
+    return in;
+
 }
 
-void AskforData(NameAmount accounts)
+
+
+
+
+// fills the array with numbers between 0 and 99
+
+// s is the array to be filled, l is length of the array
+
+
+
+int generate(int s[20], const int l)
+
 {
-	cout << "\nplease enter customer data\n"
-		<< "please enter customer data below\n\n";
-	cout << "enter amount\n";
-	cin >> accounts.CustomerAmountArray[accounts.NumberOfCustomers];
-	cout << "enter name\n";
-	cin >> accounts.CustomerNameArray[accounts.NumberOfCustomers];
 
-	cout << accounts.CustomerNameArray[accounts.NumberOfCustomers] << " and " << accounts.CustomerAmountArray[accounts.NumberOfCustomers] << "\n";
-	accounts.NumberOfCustomers++;
-	showData(accounts);
+    int num = 0;
 
-}
+    for (int i = 0; i < l; i++)
 
-int main() {
+    {
 
-	NameAmount accounts;
-	OverWriteData(accounts);
-	int input = 10;
-	cout << "hello this app is our costumer database,\n to enter a new customer press 1.\n to see a list of costumers press 2\n\n to quit select 0\n\n";
-	cin >> input;
-	while (input != 0)	
-		if (input == 1)
-		{
-			AskforData(accounts);
-		}
-		else if (input == 2)
-		{
-			showData(accounts);
-		}
-		else
-		{
-			input = 0;
-			OverWriteData(accounts);
-		}
-			return 0;
+        num = rand() % 100;
+
+        s[i] = num;
+
+        cout << num << " ";
+
+    }
+
+    return s[l];
 
 }
 
 
+
+
+
+// Prints array given
+
+// s is array to be print, l is length of array
+
+
+
+void print(int s[20], const int l)
+
+{
+
+    cout << "Printing...\n";
+
+    
+
+    for (int i = 0; i <= l; i++)
+
+    {
+
+        cout << s[i] << " ";
+
+    } 
+
+    
+
+    cout << "\nEnd Print Statement.\n";
+
+}
+
+
+
+
+
+// calls sort algorithm to sort array
+
+// s is array to sort, l is length of array
+
+
+
+int sort(int s[20], const int l)
+
+{
+
+    cout << "Sorting array...\n";
+
+    sort(s, s + l);
+
+    return s[l];
+
+}
