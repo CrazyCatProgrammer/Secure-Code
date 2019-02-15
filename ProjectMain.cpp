ebeb5db563eb78cabc47e4801e44118c3886bedc
Roxa <meowcollins@gmail.com>
2019-02-14 22:29:10 -0700

    Fixed minor Bugs
    Used FlushInputBuffer in Data function.
    
    Stopped double printing of menu

--- ProjectMain.cpp
+++ ProjectMain.cpp
@@ -1,13 +1,11 @@
 /*
  * Project.cpp
- *
  *  Created on:
  *      Author: Paul Grubb
  *		Edited by: Robyn Collins & David Bonney
  */
 
 #define fstackprotectorall //stack canary turned on for all functions. A bit overkill, but works.
-#pragma warning(disable : 4996)
 #include "Customer.h"
 #include "CustomerRewards.h"
 #include <iostream>
@@ -19,48 +17,43 @@ void AskForData(CustomerRewards &accounts);
 void ShowData(CustomerRewards &accounts);
 void FlushInputBuffer(CustomerRewards);
 
-void FlushInputBuffer(CustomerRewards)
+void FlushInputBuffer()
 {
-	// needs work
-	//int character;
 	char character;
 	while ((character = getchar()) != '\n' && character != EOF);
 }
+
 void ShowData(CustomerRewards &accounts) //reworked by David
 {
 	cout << "Showing Customer Data:\n";
 	accounts.printAll();
-	cout << "\nYour best Customer is:\n";
+	cout << "\nYour best Customer is:\n\n";
 	accounts.printBest();
 }
 
 
 void AskForData(CustomerRewards &accounts) {
 	Customer tempCust;
-	char holder[10]; // for name
-	char holy[6]; //for price
+	char tempName[20]; // for name
+	char tempPrice[6]; //for price
 	float price = 0.0;
 	string name = "";
-	//cin.clear();
 	cout << "Please enter data for a customer:\n";
 	cout << "Name:\n";
-	fgets(holder, 10, stdin); //Tip from paul, fgets is WAY easier than .get
-
-	//cin.get(holder, 10); //cuts off 10 characters for name. 10 should be plenty
-	//cin.ignore(1000, '\n'); //it skips first getline, so ignore used
-	name = holder;
+	fgets(tempName, 20, stdin); //Tip from paul, fgets is WAY easier than .get
+	name = tempName;
 	tempCust.setName(name);
+	FlushInputBuffer(); //Thanks paul for the code!
 	while (price <= 0.0) //uses 0.0 sentinel value, as well as default of strtof to prevent non-inputs
 	{
 		cout << "Please enter price:\n";
 		cin.clear();
-		cin.get(holy, 6); //6 characters should be enough for a float
-		cin.ignore(1000, '\n');
-		price = strtof(holy, NULL); //will store 0.0 if invalid conversion
+		fgets(tempPrice, 6, stdin);
+		price = strtof(tempPrice, NULL); //will store 0.0 if invalid conversion
 	}
 	tempCust.setPrice(price);
 	accounts.add(tempCust);
-
+	FlushInputBuffer();
 }
 
 int main() {
@@ -69,7 +62,7 @@ int main() {
 	char input = '3'; //initialize with an innocent value
 	int errorCount = 0;
 	while (errorCount < 3) {
-		cout << "Hello this app is our costumer database, to enter a new customer press 1.\nTo see a list of costumers press 2\nPress 0 to exit.\n";
+		cout << "\nHello this app is our costumer database, to enter a new customer press 1.\nTo see a list of costumers press 2\nPress 0 to exit.\n";
 		cin.get(input);
 		cin.ignore(1000, '\n');
 		cin.clear();
