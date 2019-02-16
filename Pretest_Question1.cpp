ccebbd22ef30081cc7cd4354922342f51228ea78
Rae <TheMidnightMage@users.noreply.github.com>
2019-02-15 22:49:24 -0700

    Question 1 Final for 2-15-19
    works but misses some of the requirements (nullptr, etc)

--- Pretest_Question1.cpp
+++ Pretest_Question1.cpp
@@ -1,104 +1,119 @@
 //============================================================================
+//
 // Name        : Pretest_Question1.cpp
 // Author      : Bonnie Rogers, Carson Sharpless
 // Version     : 1.0.0
-// Date		   : February 7th, 2019
+// Date        : February 7th, 2019
 // Description : generates a sequence of twenty random values between 0 and 99 in an array.
 //				 Print the sequence, sort the array, and print the sorted sequence.
-// Functions   : zzzz
+// Functions   : int generate(int string[20 length], constant int length) - Generates numbers to fill array
+//               void print(int string[20 length], constant int length) - prints out array
+//               int sort(int string[20 length], constant int length) - sorts numbers in array
+//               int destroyArray(int string[20 length], constant int length) - sets array to NULL to erase data
+//
 //============================================================================
 
+
+#define fstackprotectorall //stack canary 
 #include <iostream>
+#include <string>
 #include <algorithm>  	// for sort function
-#include <stdlib.h>		// rand function
+#include <stdlib.h>	// rand function
 using namespace std;
 
-/////////////////////////////////////////// Function Call ///////////////////////////////////////////
-wchar_t generate(wchar_t s, int l);
-void print(wchar_t s, int l);
-wchar_t sort(wchar_t s, int l);
 
-//create and destroy
-wchar_t createArray(int l);
-wchar_t destroyArray(wchar_t in, int l);
+/////////////////////////////////////////// Function Call ///////////////////////////////////////////
+
+int generate(int s[20], const int l);
+void print(int s[20], const int l);
+int sort(int s[20], const int l);
+int destroyArray(int in, const int l);
+
 
 /////////////////////////////////////////// Main ///////////////////////////////////////////
+
 int main()
 {
+    //setting constant size for array
+    const int l = 20;
+    int s[l];
+    
+    //static function pointer for print
+    static void(*funcPrint)(int s[20], const int l);
+    funcPrint = &print;
+    
+    //creating array
+    s[l] = generate(s, l);
+    //print original array unsorted
+    print(s, l);
+    //sort array
+    sort(s, l);
+    //print array sorted
+    print(s, l); 
+    //set array to null
+    destroyArray(s[l], l);
+    
+    //ending program
+    char end = NULL;	//character to end program
+    cout << "End Program - Press any key to continue... ";
+    cin >> end;
+    
+    return 0;
 
-
-	//static function pointer for print
-	static void (*funcPrint)(wchar_t s, int l);
-	funcPrint = &print;
-
-	//ending program
-	char end = "";	//character to end program
-	cout << "End Program. Press any key to continue... ";
-	cin >> end;
-	return 0;
 }
 
 /////////////////////////////////////////// Functions ///////////////////////////////////////////
 
-// fills the array with numbers between 0 and 99
-// s is the array to be filled, l is length of the array
-wchar_t generate(wchar_t s, int l)
-{
-
-	for(int i = 0; i < l; i++)
-	{
-		int num = rand() % 100;
-		itoa(num, s, i);
-	}
-	return s;
-}
-
-// Prints array given
-// s is array to be print, l is length of array
-void print(wchar_t s, int l)
-{
-	cout << "Printing...\n";
-	for(int i = 0; i <= l; i++)
-	{
-		if(s[i] == nullptr)
-		{
-			//skip nulls
-		}
-		else
-		{
-			cout << s[i] << " ";
-		}
-	}
-	cout << "\nEnd Print Statement.\n";
-}
-
-// calls sort algorithm to sort array
-// s is array to sort, l is length of array
-wchar_t sort(wchar_t s, int l)
-{
-	cout << "Sorting array...\n";
-	sort(s, s+l);
-	return s;
-}
-
-//create and destroy arrays
-// l is length of array to create
-wchar_t createArray(int l)
-{
-	cout << "Creating Array of " << l << " length...\n";
-	wchar_t array[l];
-
-	cout << "Generating numbers and putting into array...\n";
-	generate(array, l);
-
-	return array;
-}
 
 // sets array to null
 // in is array to set null
-wchar_t destroyArray(wchar_t in, int l)
+
+int destroyArray(int in, int l)
 {
-	cout << "Setting array to null...\n";
-	in = nullptr;
-	return in;
+    cout << "Setting array to null...\n";
+    in = NULL;
+    return in;
+}
+
+
+// fills the array with numbers between 0 and 99
+// s is the array to be filled, l is length of the array
+
+int generate(int s[20], const int l)
+{
+    cout << "Generating numbers and putting into array...\n";
+    int num = 0;
+    for (int i = 0; i < l; i++)
+    {
+        num = rand() % 100;
+        s[i] = num;
+    }
+    return s[l];
+}
+
+
+// Prints array given
+// s is array to be print, l is length of array
+
+void print(int s[20], const int l)
+{
+    cout << "Printing...\n";
+    
+    for (int i = 0; i <= l; i++)
+    {
+        cout << s[i] << " ";
+    } 
+    
+    cout << "\nEnd Print Statement.\n";
+}
+
+
+// calls sort algorithm to sort array
+// s is array to sort, l is length of array
+
+int sort(int s[20], const int l)
+{
+    cout << "Sorting array...\n";
+    sort(s, s + l);
+    return s[l];
 }
