0a469ed1e6bf08b0d9d0411577b0b95cb1989bad
DrQuack <47011925+DrQuack@users.noreply.github.com>
2019-05-01 16:25:28 -0600

    Updating Files

--- SecureGui.h
+++ SecureGui.h
@@ -1,3 +1,12 @@
+/*
+DISCLAIMER!!!
+This program has created a GUI using the Qt software, liscened under open source.
+In compliance with (L)GPL regulations, this banner is the acknowledgement of Qt usage. For more information, visit https://www.qt.io/
+This software is, and will remain, free of DRM locks and patents in compliance with these regulations.
+This software is developed for educationial purposes as part of a project at Red Rocks Community College class CSC 245 040 Spring 2019
+The students involved are Robyn Collins, Carson Sharpless
+*/
+
 #pragma once
 
 #include <QtWidgets/QMainWindow>
@@ -13,8 +22,10 @@ public:
 	SecureGui(QWidget *parent = Q_NULLPTR);
 	Reservation getReservation() { return reservation; } //added by David Bonney to allow info to be stored and transfered to main
 
-private:
-	Ui::SecureGuiClass ui;
-	void on_pushButton_clicked();
+private slots:
+	void on_pushButton_clicked(); //moved to slots for use with GUI
+	void person_pushButton_clicked();
+private: 
+	Ui::SecureGuiClass ui;	
 	Reservation reservation; //added by David Bonney to allow info to be stored and transfered to main
 };
