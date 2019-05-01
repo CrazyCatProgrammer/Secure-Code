83bbc79578f22b590c0df4ccce64bf3459901f53
csharpless <47228057+csharpless@users.noreply.github.com>
2019-04-30 19:58:15 -0600

    Updated running code

--- SecureGui.h
+++ SecureGui.h
@@ -14,7 +14,7 @@ public:
 	Reservation getReservation() { return reservation; } //added by David Bonney to allow info to be stored and transfered to main
 
 private:
-	Ui::SecureGuiClass *ui;
+	Ui::SecureGuiClass ui;
 	void on_pushButton_clicked();
 	Reservation reservation; //added by David Bonney to allow info to be stored and transfered to main
 };
