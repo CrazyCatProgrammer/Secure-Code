5fb3258754387f004fef246ee18e16041c6740d9
csharpless <47228057+csharpless@users.noreply.github.com>
2019-04-30 19:55:27 -0600

    Updated running code

--- SecureGui.cpp
+++ SecureGui.cpp
@@ -1,12 +1,16 @@
 #include "SecureGui.h"
 #include "ui_SecureGui.h"
 #include "ui_Info.h"
+#include "Reservation.h"
+#include "Person.h"
+
 
 SecureGui::SecureGui(QWidget *parent) //constructor
 	: QMainWindow(parent)
 {
-	ui->setupUi(this);
+	ui.setupUi(this);
 }
+
 
 void SecureGui::on_pushButton_clicked()
 {
@@ -15,19 +19,24 @@ void SecureGui::on_pushButton_clicked()
 
 	//TODO also code buttons doing things here
 	//The following lines of code (18-) Were coded by David Bonney
-	if (ui->radioButton->isChecked()) //one way is checked
+
+	if (ui.radioButton->isChecked()) //one way is checked
 		reservation.setRoundTrip(false);
-	else if (ui->radioButton_2->isChecked()) //round trip is checked
+	else if (ui.radioButton_2->isChecked()) //round trip is checked
 		reservation.setRoundTrip(true);
-	if (ui->checkBox->isChecked())
+	if (ui.checkBox->isChecked())
 		reservation.setQuality("First"); //name changed to comply with main.cpp price setter code
-	else if (ui->checkBox_2->isChecked())
+	else if (ui.checkBox_2->isChecked())
 		reservation.setQuality("Economy");
-	else if (ui->checkBox_3->isChecked())
+	else if (ui.checkBox_3->isChecked())
 		reservation.setQuality("Business"); //name changed to comply with main.cpp price setter code
-	int bags = ui->spinBox_2->value();
+	int bags = ui.spinBox_2->value();
+
 	reservation.setBags(bags);
 
+
+
 	//TODO: need destination, nonstop, people thing (might add a counter for use in transfering info)
+
 
 }
